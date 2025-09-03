aa6a879d246b7cf0f9f1bcfeea1a43bcac393b0c
Shubham Agarwal <shubham.agarwal480@gmail.com>
2025-09-03 16:40:10 +0530

    added a new greet endpoint

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ def hello():
     content = "this world is so amazing and it is just is 🌍🌍🌌"
     return f"Hello, DevOps World! and {content} 🚀"
 
+@app.route("/greet/:id")
+def hi(id):
+    return f"Hello, {id}"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
  
