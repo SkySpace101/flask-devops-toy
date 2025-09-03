6b8d12f65a088dad2e62b26faffca611583e6098
Shubham Agarwal <shubham.agarwal480@gmail.com>
2025-09-03 16:35:08 +0530

    added content data

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, DevOps World! 🚀"
+    content = "this world is so amazing and it is just is 🌍🌍🌌"
+    return f"Hello, DevOps World! and {content} 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+ 
