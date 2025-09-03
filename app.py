068bdf6b44ae783fd63ad4a5c90264ec737efb97
Shubham Agarwal <shubham.agarwal480@gmail.com>
2025-09-03 16:45:13 +0530

    corrected the syntax of new added end point.

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ def hello():
     content = "this world is so amazing and it is just is 🌍🌍🌌"
     return f"Hello, DevOps World! and {content} 🚀"
 
-@app.route("/greet/:id")
-def hi(id):
-    return f"Hello, {id}"
+@app.route("/greet/<name>")
+def hi(name):
+    return f"Hello, {name}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
